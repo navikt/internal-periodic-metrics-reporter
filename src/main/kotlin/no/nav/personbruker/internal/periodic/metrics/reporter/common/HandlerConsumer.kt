3abf9ed2ac9db37daf6441d4b140d70588812fc9
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-09-29 16:13:14 +0200

    Legger på en error melding så vi fanger dette opp i loggene.

--- src/main/kotlin/no/nav/personbruker/internal/periodic/metrics/reporter/common/HandlerConsumer.kt
+++ src/main/kotlin/no/nav/personbruker/internal/periodic/metrics/reporter/common/HandlerConsumer.kt
@@ -3,16 +3,19 @@ package no.nav.personbruker.internal.periodic.metrics.reporter.common
 import io.ktor.client.*
 import no.nav.personbruker.internal.periodic.metrics.reporter.config.EventType
 import no.nav.personbruker.internal.periodic.metrics.reporter.config.get
+import org.slf4j.LoggerFactory
 import java.net.URL
 
 class HandlerConsumer(private val client: HttpClient, eventHandlerBaseURL: URL) {
 
     private val pathToEndpoint = "${eventHandlerBaseURL.path}/fetch/grouped/systemuser"
+    private val log = LoggerFactory.getLogger(HandlerConsumer::class.java)
 
     suspend fun getEventCount(eventtype: EventType): Map<String, Int> {
         try {
             return client.get(URL("$pathToEndpoint/${eventtype.eventType}"))
         } catch (e: Exception) {
+            log.error("Får ikke kontakt med dittnav-event-handler. Setter derfor produsentnavnet til <appnavn_unavailable> og antall event = 0.")
             return mapOf("appnavn_unavailable" to 0)
         }
     }
